bd1a13a6472ccfdbbc99f8eab7aa4b960b81cb04
harsh.sahay <harsh.sahay@rodio.in>
2020-01-02 11:53:27 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Olya:w4abYhJqcJWqFv6@ds143893.mlab.com:43893/mern_shopping'
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const items = require('./routes/api/items');
+const app = express();
+
+//Body-parser middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongoDB
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+//Use routes
+app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    });
+
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
